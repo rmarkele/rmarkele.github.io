e747b91517c161e00ff287ec3b886f996b14a53d
Renato Markele <renatomarkele@hotmail.com>
2024-08-18 18:33:51 -0300

    pipe in range position

--- src/mouseEvents/pipeSliderCreate.js
+++ src/mouseEvents/pipeSliderCreate.js
@@ -1,18 +1,20 @@
 const pipeSlider = document.getElementById("pipe-slider");
 
 function pipeSliderCreate() {
-    let posx =  canoEntradaTanque2.posx + (canoEntradaTanque2.largura) / 2;
-    let posy = tanqueRep.posy + 36;
-    let hgt = altura - nivel0 - hrep + 72;
-    pipeSlider.style.left = posx + "px";
-    pipeSlider.style.top = posy + "px";
-    pipeSlider.style.width = hgt + "px";
-    
+  let posx =
+    canvasW < respLim[1]
+      ? joelho3.posx + (joelho3.largura + canoEntradaTanque2.largura) / 2
+      : canoEntradaTanque2.posx + canoEntradaTanque2.largura / 2;
+  let posy = tanqueRep.posy + 36;
+  let hgt = altura - nivel0 - hrep + 72;
+  pipeSlider.style.left = posx + "px";
+  pipeSlider.style.top = posy + "px";
+  pipeSlider.style.width = hgt + "px";
 }
 
-pipeSlider.addEventListener("input", ()=>{
-    let posy = map(pipeSlider.value, 0, 100, tanqueRep.posy, yref - altura);
-    hsaida = altura - nivel0 - hrep - posy + joelho3.w;
-    hsaida = constrain(hsaida, 0, altura - nivel0 - hrep)
-    updateRepTank()
-})
+pipeSlider.addEventListener("input", () => {
+  let posy = map(pipeSlider.value, 0, 100, tanqueRep.posy, yref - altura);
+  hsaida = altura - nivel0 - hrep - posy + joelho3.w;
+  hsaida = constrain(hsaida, 0, altura - nivel0 - hrep);
+  updateRepTank();
+});
